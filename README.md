4ee4489429971e7a4d17e42fccc4095005e7bbfd
Kiernan2 <kiernan1994@gmail.com>
2021-08-05 15:21:22 -0700

    Completed business logic

---
+++ README.md
@@ -0,0 +1,23 @@
+describe romanNumerals();
+
+Code: romanNumerals(2);
+Expected Output: "II";
+
+Code: romanNumerals(66);
+Expected Output: "LXVI"
+
+Code: romanNumerals(99);
+Expected Output: "XCIX"
+
+Code: romanNumerals(90);
+Expected Output: "XC"
+
+Code: romanNumerals(3999);
+Expected Output: "MMMCMXCIX"
+
+Test: make sure the numbers between 1 and 3999
+Code: romanNumerals(4000)
+Expected Output: "Error"
+
+Code: romnaNumerals(0)
+Expected Output: "Error"
---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// Business Logic
+function romanNumerals(number) {
+  
+  const symbols = ["M",   "CM", "D",  "CD",  "C", "XC","L", "XL","X","IX","V","IV","I"]
+  const numbers = ["1000","900","500","400","100","90","50","40","10","9","5","4","1"]
+  let answer = ""
+  
+  if (number > 3999 || number < 1 || typeof(number) !== "number") {
+    return "Error";
+  }
+
+  for(let i = 0; i < numbers.length; i++) {
+    if (number >= numbers[i]) {
+      number = number - numbers[i];
+      answer = answer + symbols[i];
+      i = -1
+    }
+  }
+  return answer;
+}
+
+console.log(romanNumerals(49))
+
+// UI Logic
+
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     event.preventDefault();
+//     let input = ($("input#input").val());
+//     $("#output").append(romanNumerals(input));
+//   })
+// })
